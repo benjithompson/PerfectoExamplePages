d06c0d783dfbb1a5ac5a8c5182f95042072441d0
Ben Thompson <toobenjit@gmail.com>
2025-09-30 14:41:23 -0700

    Refactor w2summary.html for improved design and layout; add new charts.html for stock market data visualization with responsive charts and data tables.

--- examples/examples-list.js
+++ examples/examples-list.js
@@ -6,6 +6,7 @@ window.EXAMPLES = [
   { file: 'orderform.html', name: 'Order Form' },
   { file: 'elements.html', name: 'UI Elements' },
   { file: 'store.html', name: 'Store Showcase' },
+  { file: 'charts.html', name: 'Charts' },
   { file: 'w2.html', name: 'W-2 Tax Form' },
   { file: 'http://dbankdemo.com/bank', name: 'Bank Demo' },
   { file: 'https://blazedemo.com', name: 'Blaze Travel' },
